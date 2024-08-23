c247a2a4e51f20194e6863e3708a4acea7fd160f
Niluba <139118395+Niluba@users.noreply.github.com>
2024-08-23 16:19:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# getting-started-with-the-vue-in-place-editor-component
+# Getting Started with the Vue In-place Editor Component
+
 A quick-start project that contains code to create and configure the Syncfusion Vue In-place Editor component. This project also includes a code snippet to integrate other components like the DatePicker, TextBox, and Dropdown List and how to change the editor mode.
+ 
+Refer to the following documentation to learn about the Vue In-place Editor component: 
+https://ej2.syncfusion.com/vue/documentation/inplace-editor/vue3-getting-started  
+
+Check out this online example of the Vue In-place Editor component:
+https://ej2.syncfusion.com/vue/demos/#/material/inplace-editor/default.html 
+
+## Project prerequisites
+Make sure that you have the compatible versions of [Visual Studio Code](https://code.visualstudio.com/download ) and [NodeJS](https://nodejs.org/en/download) or later version in your machine before starting to work on this project.
+
+## How to run this application
+To run this application, you need to first clone the `getting-started-with-the-vue-in-place-editor-component` repository and then open it in Visual Studio Code. Now, simply build and run your project to view the output.
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { InPlaceEditorComponent as EjsInplaceeditor } from "@syncfusion/ej2-vue-inplace-editor";
+
+const textModel =  { placeholder: 'Enter your name'};
+const dateModel= {placeholder: 'Select date of birth'};
+const dateValue= new Date('5/23/2017')
+const genderData = ['Male', 'Female'];
+const genderModel= {  dataSource: genderData, placeholder: 'Select a gender' };
+</script>
+
+<template>
+<div class="container">
+ <h4> Modify the details </h4>
+  <table>
+  <tr>
+      <td>Name</td>
+      <td>
+        <ejs-inplaceeditor type="Text" :model="textModel"
+        value="Andrew">
+        </ejs-inplaceeditor>  
+      </td>
+  </tr>
+  <tr>
+      <td>Date of Birth</td>
+      <td>
+        <ejs-inplaceeditor type="Date"
+        :value="dateValue" :model="dateModel">
+        </ejs-inplaceeditor>
+      </td>
+  </tr>
+  <tr>
+      <td>Gender</td>
+      <td>
+        <ejs-inplaceeditor type="DropDownList" :model="genderModel"
+        mode="Inline" data-underline="false">
+        </ejs-inplaceeditor>
+      </td>
+  </tr>
+  </table>
+  </div>
+</template>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css"; 
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inplace-editor/styles/material.css";
+
+.container {
+  text-align:left;
+  margin-bottom: 60%;
+} 
+</style>
